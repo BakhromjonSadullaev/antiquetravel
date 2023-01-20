2941dd8e17dba0266b0ca4b1aafd5c2429d9dcf1
Mirzoyoqub <mirzoyoqub123@gmail.com>
2023-01-20 18:30:46 +0500

    fixed issues

--- src/components/UsefulLinks/components/Link.jsx
+++ src/components/UsefulLinks/components/Link.jsx
@@ -3,12 +3,12 @@ import style from "./Link.module.scss";
 import { Link } from "react-router-dom";
 const Linker = ({ icon, title, link }) => {
   return (
-    <a href={link} target="_blank" className={style.body}>
+    <Link to={link} target="_blank" className={style.body}>
       <div className={style.container}>
         {icon}
         <h3>{title}</h3>
       </div>
-    </a>
+    </Link>
   );
 };
 
