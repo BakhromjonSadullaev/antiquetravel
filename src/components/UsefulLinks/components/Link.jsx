e7278c987727b033ec1078abc994274226ee2e8a
Mirzoyoqub <mirzoyoqub123@gmail.com>
2023-01-20 19:59:30 +0500

    feat: made some changes

--- src/components/UsefulLinks/components/Link.jsx
+++ src/components/UsefulLinks/components/Link.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import style from "./Link.module.scss";
 import { Link } from "react-router-dom";
 const Linker = ({ icon, title, link }) => {
-  return (
-    <Link to={link} target="_blank" className={style.body}>
+  return link === "payment" ? (
+    <Link to={link} className={style.body}>
       <div className={style.container}>
         {icon}
         <h3>{title}</h3>
       </div>
     </Link>
+  ) : (
+    <a href={link} target="_blank" className={style.body}>
+      <div className={style.container}>
+        {icon}
+        <h3>{title}</h3>
+      </div>
+    </a>
   );
 };
 
